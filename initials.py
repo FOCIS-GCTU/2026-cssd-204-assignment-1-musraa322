4966f6e7755a40a7eb6c7295672b9c637ce163a8
musraa322 <ramatumusah652@gmail.com>
2026-05-31 12:10:53 +0000

    Add files via upload
    scripting language assignment

--- initials.py
+++ initials.py
@@ -1,7 +1,36 @@
 
-def main():
-  pass #code goes here
+File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Ramatu Musah
+# STUDENT ID: 2425403168
+# Email: ramatumusag652@gmail.com
+# Grader: Carolyn
+#
+# On my honor, Ramatu Musah, this programming assignment is my own work
+# and I have not provided this code to any other student.
 
+
+def main():
+    # Print my initials in large stylized letters.
+
+    print()
+    print("...RMM")
+    print()
+
+    print("...RRRRRRRRRRRR...MMMM......MMMM...MMMM......MMMM...**")
+    print("...RRRRRRRRRRRR...MMMMM....MMMMM...MMMMM....MMMMM...**")
+    print("...RRR......RRR...MM.MM..MM.MM...MM.MM..MM.MM...**")
+    print("...RRR......RRR...MM..MMMM..MM...MM..MMMM..MM...**")
+    print("...RRRRRRRRRRRR...MM...MM...MM...MM...MM...MM...**")
+    print("...RRRRRRRRRR....MM........MM...MM........MM...**")
+    print("...RRR...RRR.....MM........MM...MM........MM...**")
+    print("...RRR....RRR....MM........MM...MM........MM...**")
+    print("...RRR.....RRR...MM........MM...MM........MM...**")
+    print("...RRR......RRR..MM........MM...MM........MM...**")
+
+    print()
 
 
 main()
